d94e88e2de1a611c8e7233b89baae61d8dd1dce7
Dilip Instahire <dilip@instahire.us>
2022-08-07 15:37:18 +0530

    text hide

--- complete/lecture3_2/VRButton.js
+++ complete/lecture3_2/VRButton.js
@@ -155,7 +155,7 @@ class VRButton{
         button.style.right = '0px';
         button.style.bottom = '0px';
         button.style.border = '';
-        button.style.opacity = '1';
+        button.style.opacity = '0';
         button.style.fontSize = '13px';
         button.textContent = 'VR NOT SUPPORTED1';
 
